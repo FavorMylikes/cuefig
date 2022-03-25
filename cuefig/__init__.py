5a13133ef0bf2d271f27c940fd4c004a27843578
Administrator <>
2022-03-25 12:46:05 +0800

    init

--- cuefig/__init__.py
+++ cuefig/__init__.py
@@ -1,5 +1,18 @@
 import sys
 
+from cuefig.utils import eprint
 
-def eprint(*args, **kwargs):
-    print(*args, file=sys.stderr, **kwargs)
+try:
+    from conf import *
+except ImportError:
+    eprint("You dont have create conf package")
+
+try:
+    from conf.config import *
+except ImportError:
+    eprint("You dont have create conf/config.py file")
+
+try:
+    from conf.config_deploy import *
+except ImportError:
+    eprint("You dont have create conf/config_deploy.py file")
