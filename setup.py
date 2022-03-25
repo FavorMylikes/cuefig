985214619207c73a0aafa3874a5d5c77aa15ea8b
Administrator <>
2022-03-25 14:42:43 +0800

    add install requires

--- setup.py
+++ setup.py
@@ -5,13 +5,13 @@ with open('requirements.txt', mode="r", encoding="utf8") as f:
 
 setup(
     name='cuefig',
-    version='0.0.1',
+    version='0.0.2',
     url='https://github.com/FavorMylikes/cuefig',
     license='MIT License',
     author='麦丽素',
     author_email='l786112323@gmail.com',
     description='A config framework that you can cue and hint quickly.',
-    data_files=[('cuefig/logger', ['cuefig/logger/logging.yaml'])],
+    package_data={"": ["*.yaml"], },
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
